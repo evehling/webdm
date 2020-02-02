45df088daafd59c3932b17ea0bd7f637d7922b75
Mark Delarosa <delarosa.mark.j@gmail.com>
2020-02-01 18:18:42 -0800

    Updated html/css to include overlay

--- public/content.js
+++ public/content.js
@@ -1,2 +1,30 @@
 // functionality for chrome extension
- alert("You've successfully used cloud extension, congratz!");
+//  alert("You've successfully used cloud extension, congratz!");
+
+// var enterBtn = document.getElementsByClassName("gLFyf");
+// document.getElementsByClassName("")
+// alert("Accessed the DOM");
+function overlayOn(){
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff(){
+    document.getElementById("overlay").style.display = "none";
+}
+
+searchArr = document.getElementsByClassName("gLFyf");
+
+// prompts user when pressing enter
+document.getElementById("tsf").addEventListener("submit", 
+
+    event => { alert("stop pressing the button"),
+    alert(searchArr[0].value),
+    // overlayOn()
+    document.getElementById("overlay").style.display = "block",
+    event.preventDefault()
+
+});
+
+
+// console.log(searchArr[0].value);
+// chrome.tabs.query
