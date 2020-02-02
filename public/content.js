4f580eb98ed26a7b454bc3f5a1af14e8f5c9eb2a
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-02-02 02:04:46 -0800

    Created fetch request to backend

--- public/content.js
+++ public/content.js
@@ -1,20 +1,42 @@
 // functionality for chrome extension
 
 function getSearchQuery() {
-  let form = document.getElementById("tsf");
-  let input = document.getElementsByClassName("gLFyf")[0];
-      
-  form.addEventListener("submit", e => {
-      e.preventDefault();
-      // create a new div element 
-      let newDiv = document.createElement("div"); 
-      let newContent = document.createTextNode(input.value); 
+   let input = document.getElementsByClassName("gLFyf")[0];
+ 
+   if(input.value.length) {
+     // create a new div element 
+     let newDiv = document.createElement("div"); 
+     let newContent = document.createTextNode(input.value); 
+ 
+     newDiv.appendChild(newContent);  
+     newDiv.style.position = "absolute";
+     newDiv.style.height = "100vh";
+     newDiv.style.width = "100vw";
+     newDiv.style.background = "black";
+ 
+      console.log("making api call");
+     //document.getElementsByTagName("body")[0].appendChild(newContent);
+      fetch("https://73f816ea.ngrok.io",
+        {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json'
+            //'Content-Type': 'text'
+            //'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          //body: JSON.stringify({"input": "Itchy skin"})
+          //body: "{\"input\": \"Itchy skin\"}"
+          //body: "hi"
+        })
+        .then(res => {console.log(res); return res.json()})
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 
-      newDiv.appendChild(newContent);  
-      document.getElementsByTagName("body")[0].appendChild(newContent);
-  });
-}
-getSearchQuery();
+   }
+ }
+ getSearchQuery();
 
 // Gets current window url
 /*chrome.tabs.query({
